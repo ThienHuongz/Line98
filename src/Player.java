317d72d4639b32867fa989f3bba96846e4332cfe
Dinh Thi Thanh Nha <89521781+DiNha31@users.noreply.github.com>
2023-12-03 18:28:38 +0700

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,67 +1,65 @@
 package Lines;
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
-import javax.swing.JOptionPane;
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
 
-public class Player {
-   public String name;
-   public int scores;
+public class Player{
+	
+	public String name;
+	public int  scores;
+	
+	public Player(){
+		this.name = "";
+		this.scores=0;
+	}
+	public Player(String name,int scores){
+		this.name = name;
+		this.scores=scores;
+	}
+	public void setName(){
+		 String a;
+ 		 a = JOptionPane.showInputDialog(" Ten cua ban la gi vay ?")+"";
+ 		 this.name=a;
+	}	
 
-   public Player() {
-      this.name = "";
-      this.scores = 0;
-   }
+	public void writeData(DataOutput out)throws IOException{
+		writeFixedString(this.name, 20 , out);
+        out.writeInt(this.scores);
 
-   public Player(String var1, int var2) {
-      this.name = var1;
-      this.scores = var2;
-   }
+	}
+	
+	public void readData( DataInput in)throws IOException{
+		this.name = readFixedString(20, in);
+        this.scores 	    = in.readInt();
+      	
+	}	
 
-   public void setName() {
-      String var1 = JOptionPane.showInputDialog(" Ten cua ban la gi vay ?") + "";
-      this.name = var1;
-   }
-
-   public void writeData(DataOutput var1) throws IOException {
-      this.writeFixedString(this.name, 20, var1);
-      var1.writeInt(this.scores);
-   }
-
-   public void readData(DataInput var1) throws IOException {
-      this.name = this.readFixedString(20, var1);
-      this.scores = var1.readInt();
-   }
-
-   public String readFixedString(int var1, DataInput var2) throws IOException {
-      StringBuilder var3 = new StringBuilder(var1);
-      int var4 = 0;
-      boolean var5 = true;
-
-      while(var5 && var4 < var1) {
-         char var6 = var2.readChar();
-         ++var4;
-         if (var6 == 0) {
-            var5 = false;
-         } else {
-            var3.append(var6);
-         }
-      }
-
-      var2.skipBytes(2 * (var1 - var4));
-      return var3.toString();
-   }
-
-   public void writeFixedString(String var1, int var2, DataOutput var3) throws IOException {
-      for(int var4 = 0; var4 < var2; ++var4) {
-         char var5 = 0;
-         if (var4 < var1.length()) {
-            var5 = var1.charAt(var4);
-         }
-
-         var3.writeChar(var5);
-      }
-
-   }
+	public  String readFixedString(int size, DataInput in)
+	  	throws IOException{
+    	
+    	StringBuilder b = new StringBuilder(size);
+      	int i = 0;
+      	boolean more = true;
+      	while (more && i < size){
+        	char ch = in.readChar();
+         	i++;
+         	if (ch == 0)
+         		more = false;
+         	else b.append(ch);
+      	}
+      	
+      	in.skipBytes(2 * (size - i));
+      	return b.toString();
+ 	}
+	
+   	public  void writeFixedString(String s, int size, DataOutput out)
+      throws IOException{
+      	int i;
+      	for (i = 0; i < size; i++){
+        	char ch = 0;
+         	if (i < s.length()) ch = s.charAt(i);
+         	out.writeChar(ch);
+      	}
+   	}
 }
