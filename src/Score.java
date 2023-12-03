c3a47007147520c8eda500ea94122213245c4d3d
Dinh Thi Thanh Nha <89521781+DiNha31@users.noreply.github.com>
2023-12-03 18:26:35 +0700

    Update Score.java

--- src/Score.java
+++ src/Score.java
@@ -1,39 +1,65 @@
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import javax.imageio.ImageIO;
+package Lines;
 
-public class Score {
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
 
-  private BufferedImage img;
-  private BufferedImage sc[] = new BufferedImage[10];
-  private int count = 99999;
+public class Player{
+	
+	public String name;
+	public int  scores;
+	
+	public Player(){
+		this.name = "";
+		this.scores=0;
+	}
+	public Player(String name,int scores){
+		this.name = name;
+		this.scores=scores;
+	}
+	public void setName(){
+		 String a;
+ 		 a = JOptionPane.showInputDialog(" Ten cua ban la gi vay ?")+"";
+ 		 this.name=a;
+	}	
 
-  public Score() {
-    try {
-      img = ImageIO.read(new File("../assets/score.png"));
-      for (int i = 0; i < 10; i++) {
-        
-        sc[i]= img.getSubimage(0+18*i, 0, 18 , 35);
-      }
-    } catch (Exception e) {
-      // TODO: handle exception
-      e.printStackTrace();
-    }
-  }
+	public void writeData(DataOutput out)throws IOException{
+		writeFixedString(this.name, 20 , out);
+        out.writeInt(this.scores);
 
-  public void draw(Graphics g) {
-    int n = count;
-    for (int i=5;i>0;i--){
-      int j = n % 10;
-      n/=10;
-      g.drawImage(sc[j], 20 + 20 * i, 10,  null);      
-      g.drawImage(sc[j], 250 + 20 * i, 10,  null);
+	}
 
-    }
-  }
-  public void setCount(){
-    this.count=count+1;
-  }
-  
+	public void readData( DataInput in)throws IOException{
+		this.name = readFixedString(20, in);
+        this.scores 	    = in.readInt();
+      	
+	}	
+	
+	public  String readFixedString(int size, DataInput in)
+	  	throws IOException{
+    	
+    	StringBuilder b = new StringBuilder(size);
+      	int i = 0;
+      	boolean more = true;
+      	while (more && i < size){
+        	char ch = in.readChar();
+         	i++;
+         	if (ch == 0)
+         		more = false;
+         	else b.append(ch);
+      	}
+      	
+      	in.skipBytes(2 * (size - i));
+      	return b.toString();
+ 	}
+	
+   	public  void writeFixedString(String s, int size, DataOutput out)
+      throws IOException{
+      	int i;
+      	for (i = 0; i < size; i++){
+        	char ch = 0;
+         	if (i < s.length()) ch = s.charAt(i);
+         	out.writeChar(ch);
+      	}
+   	}
 }
